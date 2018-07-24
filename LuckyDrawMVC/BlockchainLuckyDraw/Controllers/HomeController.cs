359ea75e30cf949fbff41330d77409b078f15bcd
Simon Wu <siw@microsoft.com>
2018-07-24 16:48:10 +0800

    use hash for next round

--- LuckyDrawMVC/BlockchainLuckyDraw/Controllers/HomeController.cs
+++ LuckyDrawMVC/BlockchainLuckyDraw/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BlockchainLuckyDraw.Models;
+using System.Security.Cryptography;
+using System.Numerics;
+using System.Text;
 
 namespace BlockchainLuckyDraw.Controllers
 {
@@ -16,6 +19,8 @@ namespace BlockchainLuckyDraw.Controllers
 
         private static List<string> histories = new List<string>();
 
+        private static MD5 md5 = MD5.Create();
+
         private Random seed = new Random();
 
         private int LuckyCount = 10;
@@ -25,32 +30,40 @@ namespace BlockchainLuckyDraw.Controllers
             return View();
         }
 
-        private int NextRandomNumber(ref string history)
+        private int GetNumber(byte[] data)
         {
-            var newNumber = seed.Next(N);
-            while (drawedNumbers.Contains(newNumber))
+            return (int)(BitConverter.ToUInt32(data) % N);
+        }
+
+        private int NextRandomNumber(string lastInput, long time, ref string history)
+        {
+            var nextInput = md5.ComputeHash(Encoding.UTF8.GetBytes(lastInput + time.ToString()));
+            var nextNumber = GetNumber(nextInput);
+            while (drawedNumbers.Contains(nextNumber))
             {
-                history += $"-{newNumber}, ";
-                newNumber = seed.Next(N);
+                history += $"-{nextNumber}, ";
+                nextInput = md5.ComputeHash(Encoding.UTF8.GetBytes(nextInput + time.ToString()));
+                nextNumber = GetNumber(nextInput);
             }
 
-            history += $"+{newNumber}, ";
+            history += $"+{nextNumber}, ";
 
-            drawedNumbers.Add(newNumber);
-            return newNumber;
+            drawedNumbers.Add(nextNumber);
+            return nextNumber;
         }
 
         public IActionResult Draw(int drawNumber)
         {
             LuckyCount = drawNumber;
-            int[] luckyNumbers = new int[LuckyCount];
+            int[] luckyNumbers = new int[LuckyCount + 1];
             string history = "";
-            for (int i = 0; i < LuckyCount; i++)
+            luckyNumbers[0] = seed.Next(N);
+            for (int i = 1; i <= LuckyCount; i++)
             {
-                luckyNumbers[i] = NextRandomNumber(ref history);
+                luckyNumbers[i] = NextRandomNumber(luckyNumbers[i-1].ToString(), DateTime.UtcNow.Millisecond, ref history);
             }
 
-            luckyNumbers = luckyNumbers.OrderBy(n => n).ToArray();
+            luckyNumbers = luckyNumbers.Skip(1).OrderBy(n => n).ToArray();
             histories.Add(history);
             return View("Index", new LuckyNumberModel()
             {
